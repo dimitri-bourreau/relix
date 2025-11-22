6b66360c1456536a89e27eb2b019102463aeb0fc
Dimitri Bourreau <contact@dimitribourreau.me>
2025-11-22 19:48:01 +0100

    fix(mock): add videoPosterUrl to mockVideoData

--- features/video/mocks/video-data.mock.ts
+++ features/video/mocks/video-data.mock.ts
@@ -4,6 +4,8 @@ export const mockVideoData: VideoData = {
   contributor: "Signes de sens",
   id: 189151,
   source: "http://dictionnaire.sensagent.com/SOLEIL/fr-fr/",
+  videoPosterUrl:
+    "https://elix-lsf.s3.rbx.io.cloud.ovh.net/spip_images/a373c3c0d12016a580094a0934049566.jpg",
   videoUrl:
     "https://elix-lsf.s3.rbx.io.cloud.ovh.net/spip_videos/91_soleil_1__nm_1td001-encoded.mp4",
   word: "soleil",
